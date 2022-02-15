66cfd665920eaae34acf77c0e83192f58494d673
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-02-15 12:14:47 +0000

    data

---
+++ seeds/data/thoughts.js
@@ -0,0 +1,97 @@
+const thoughts = [
+    {
+      thoughtText: "NoSQL is super easy",
+      username: "Adam Khan",
+      reactions: [
+        {
+          reactionBody: "Yes I agree with you!!",
+          username: "Hassan Khan",
+        },
+        {
+          reactionBody: "Yup agree",
+          username: "Adam Khan",
+        },
+        {
+          reactionBody: "I am struggling with NoSQL",
+          username: "Bob smith",
+        },
+      ],
+    },
+    {
+      thoughtText: "NoSQL is killing me",
+      username: "bob smith",
+      reactions: [
+        {
+          reactionBody: "Yes, it is!!",
+          username: "Jimmy smith",
+        },
+        {
+          reactionBody: "Really??",
+          username: "Tony montana",
+        },
+        {
+          reactionBody: "I don't think so!!",
+          username: "Hassan Khan",
+        },
+      ],
+    },
+    {
+      thoughtText: "I love churros!!",
+      username: "bobsmith",
+      reactions: [
+        {
+          reactionBody: "Me too!!",
+          username: "jimmy smith",
+        },
+        {
+          reactionBody: "I just hate them!!",
+          username: "Tony montana",
+        },
+        {
+          reactionBody: "Best desert ever!!",
+          username: "Adam Khan",
+        },
+      ],
+    },
+    {
+      thoughtText: "I am so tired today...",
+      username: "Adam Khan",
+      reactions: [
+        {
+          reactionBody: "You need to rest!!",
+          username: "bob smith",
+        },
+        {
+          reactionBody: "Why??",
+          username: "Hassan Khan",
+        },
+        {
+          reactionBody: "I am missing my bed too!!",
+          username: "Adam Khan",
+        },
+      ],
+    },
+    {
+      thoughtText: "I hate stupid people!!",
+      username: "Tony montana",
+      reactions: [
+        {
+          reactionBody: "Take easy!!!!",
+          username: "Bob smith",
+        },
+        {
+          reactionBody: "What happened?",
+          username: "Adam kahn",
+        },
+        {
+          reactionBody: "Hope you are fine!!",
+          username: "Hassan Khan",
+        },
+      ],
+    },
+    
+  
+    
+  ];
+  
+  module.exports = thoughts;
