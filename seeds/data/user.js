cdc77989444a35dd272fbaf01df73a0c0ae5b61e
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-02-15 12:17:11 +0000

    data

---
+++ seeds/data/user.js
@@ -0,0 +1,35 @@
+
+const users = [
+    {
+      username: "bobsmith",
+      email: "bobsmith@email.com",
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: "Hassan Khan",
+      email: "HassanKhan@email.com",
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: "Adam Khan",
+      email: "Adama Khan@email.com",
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: "Tony montana",
+      email: "TonyM@email.com",
+      thoughts: [],
+      friends: [],
+    },
+    {
+      username: "Jimmy smith",
+      email: "Jimmysmith@email.com",
+      thoughts: [],
+      friends: [],
+    },
+  ];
+  
+  module.exports = users;
